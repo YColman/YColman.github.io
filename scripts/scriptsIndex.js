e16f390b6c44063be183ec5ffc3927651d3ba88b
YColman <yohann.colman@hei.yncrea.fr>
2018-10-03 08:50:42 +0200

    Fixed bugs on variables incrementation : works correctly now

--- scripts/scriptsIndex.js
+++ scripts/scriptsIndex.js
@@ -1,12 +1,13 @@
 var nbEnterPress = 0;
 
 document.addEventListener('keypress', function (e) {
+    console.log("Nombre de fois touche 'entrée': "+nbEnterPress);
     if (e.keyCode == 13) {
         if (nbEnterPress == 0) {
             afficheCalcul();
-        } else if (nbEnterPress == 1 || nbEnterPress == 2) {
+        } else if (nbEnterPress == 1 || nbEnterPress == 2 || nbEnterPress == 3) {
             afficheReponse();
-        } else if (nbEnterPress == 3) {
+        } else if (nbEnterPress == 4) {
             afficheReponse();
             nbEnterPress = -1;
         }
@@ -37,7 +38,7 @@ function afficheReponse() {
 
 
     //var essais;
-    console.log(essais);
+    console.log("Nombre d'essais: "+essais);
     var x = document.getElementById("reponseUser");
     var text;
     text = x.value;
@@ -51,7 +52,7 @@ function afficheReponse() {
         if (essais == 3) {
             document.getElementById("resultat").innerHTML = "<p style=" + "font-size:25;color:red>Raté, le résultat était " + reponse + "</p>";
             document.getElementById("boutonValiderCalcul").style.visibility = "hidden";
-            //essais = 0;
+            essais = 0;
         } else {
             document.getElementById("resultat").innerHTML = "<p style=" + "font-size:25;color:red>Raté essaie encore !</p>";
 
