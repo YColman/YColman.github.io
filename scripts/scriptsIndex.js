958f9cf6d373dbc528a24de19302bc4dce2e0a4a
YColman <yohann.colman@hei.yncrea.fr>
2018-10-03 08:32:58 +0200

    Added enter key press navigation

--- scripts/scriptsIndex.js
+++ scripts/scriptsIndex.js
@@ -1,6 +1,16 @@
+var nbEnterPress = 0;
+
 document.addEventListener('keypress', function (e) {
     if (e.keyCode == 13) {
-        afficheReponse();
+        if (nbEnterPress == 0) {
+            afficheCalcul();
+        } else if (nbEnterPress == 1 || nbEnterPress == 2) {
+            afficheReponse();
+        } else if (nbEnterPress == 3) {
+            afficheReponse();
+            nbEnterPress = -1;
+        }
+        nbEnterPress += 1;
     }
 }, false);
 
@@ -20,34 +30,33 @@ function afficheCalcul() {
 
 }
 
-var essais=0;
+var essais = 0;
 
 function afficheReponse() {
     document.getElementById("resultat").style.visibility = "visible";
-    
-    
-    
+
+
     //var essais;
     console.log(essais);
     var x = document.getElementById("reponseUser");
     var text;
-        text = x.value;
+    text = x.value;
     var reponse = document.getElementById("correction").innerHTML
-    if (reponse == text){
-        document.getElementById("resultat").innerHTML = "<p style="+"font-size:25;color:green> Bravo, vous avez trouvé ! </p>";
+    if (reponse == text) {
+        document.getElementById("resultat").innerHTML = "<p style=" + "font-size:25;color:green> Bravo, vous avez trouvé ! </p>";
         document.getElementById("reponseUser").style.visibility = "hidden";
         document.getElementById("boutonValiderCalcul").style.visibility = "hidden";
-    }else{
-    //console.log(reponse);
-        if(essais == 3){
-            document.getElementById("resultat").innerHTML = "<p style="+"font-size:25;color:red>Raté, le résultat était "+reponse+"</p>";
+    } else {
+        //console.log(reponse);
+        if (essais == 3) {
+            document.getElementById("resultat").innerHTML = "<p style=" + "font-size:25;color:red>Raté, le résultat était " + reponse + "</p>";
             document.getElementById("boutonValiderCalcul").style.visibility = "hidden";
             //essais = 0;
-        }else{
-            document.getElementById("resultat").innerHTML = "<p style="+"font-size:25;color:red>Raté essaie encore !</p>";
-            
+        } else {
+            document.getElementById("resultat").innerHTML = "<p style=" + "font-size:25;color:red>Raté essaie encore !</p>";
+
             essais = essais + 1;
-            
+
         }
     }
 
