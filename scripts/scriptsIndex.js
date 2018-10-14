a2a14292469f5b02aa3ce07eca996491af9d8e13
NTatou <noufele.tatou@hei.yncrea.fr>
2018-10-14 18:16:16 +0200

    différentes opérations avec nombres aléatoires

--- scripts/scriptsIndex.js
+++ scripts/scriptsIndex.js
@@ -23,21 +23,74 @@ document.addEventListener('keypress', function (e) {
 }, false);
 
 function afficheCalcul() {
-    document.getElementById("newCalcul").style.visibility = "hidden";
-    document.getElementById("reponseUser").value = null;
-    document.getElementById("reponseUser").style.visibility = "visible";
-    document.getElementById("boutonValiderCalcul").style.visibility = "visible";
-    document.getElementById("resultat").style.visibility = "hidden";
-    document.getElementById("correction").style.visibility = "hidden";
-    var nombre1 = Math.floor(Math.random() * 50);
-    var nombre2 = Math.floor(Math.random() * 50);
-    var correction = nombre1 + nombre2;
-    document.getElementById("calculAFaire").innerHTML = "" + nombre1 + "+" + nombre2;
-    document.getElementById("correction").innerHTML = correction;
-    document.getElementById("calculAFaire").value = "";
-    document.getElementById("reponseUser").focus();
-    essais=0;
 
+    var radios = document.getElementsByName('operation');
+
+    for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+            // do whatever you want with the checked radio
+            //alert(radios[i].value);
+            var operation = radios[i].value;
+            // only one radio can be logically checked, don't check the rest
+            break;
+        }
+    }
+    if(document.getElementById("hasard").checked){
+        
+        document.getElementById("newCalcul").style.visibility = "hidden";
+        document.getElementById("reponseUser").value = null;
+        document.getElementById("reponseUser").style.visibility = "visible";
+        document.getElementById("boutonValiderCalcul").style.visibility = "visible";
+        document.getElementById("resultat").style.visibility = "hidden";
+        document.getElementById("correction").style.visibility = "hidden";
+        var nombre1 = Math.floor(Math.random() * 50);
+        var nombre2 = Math.floor(Math.random() * 50);
+
+        switch(operation) {
+            case "+":
+                var correction = nombre1 + nombre2;
+                break;
+            case "-":
+                var correction = nombre1 - nombre2;
+                break;
+            case "x":
+                var correction = nombre1 * nombre2;
+                break;
+        }
+
+
+
+        document.getElementById("calculAFaire").innerHTML = "" + nombre1 +" "+ operation +" "+ nombre2;
+        document.getElementById("correction").innerHTML = correction;
+        document.getElementById("calculAFaire").value = "";
+        document.getElementById("reponseUser").focus();
+        essais=0;
+    }else{
+        var table = document.getElementById("table");
+        switch(table){
+            case "0":
+                for(var j = 0; j < 10; j++){
+                    var multiplicat = j;
+                    document.getElementById("newCalcul").style.visibility = "hidden";
+                    document.getElementById("reponseUser").value = null;
+                    document.getElementById("reponseUser").style.visibility = "visible";
+                    document.getElementById("boutonValiderCalcul").style.visibility = "visible";
+                    document.getElementById("resultat").style.visibility = "hidden";
+                    document.getElementById("correction").style.visibility = "hidden";
+                    
+                }
+                break;
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+        }
+    }
 
 }
 
